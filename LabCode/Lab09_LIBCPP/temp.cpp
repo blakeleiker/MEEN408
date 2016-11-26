9f5caf02a07d2283aa4db9df8469c5f3bf9db290
augustusellis <augustusellis@gmail.com>
2016-11-25 22:59:03 -0600

    Added LED Blink indicators to temp.cpp

--- LabCode/Lab09_LIBCPP/temp.cpp
+++ LabCode/Lab09_LIBCPP/temp.cpp
@@ -9,9 +9,11 @@ int main()
 	for (int i = 0; i < 10; i++) {
 		if (i % 2 == 0){
 			myGPIO.setValue(1);
+			std::cout << "LED ON" << std::endl;
 		}
 		else{
 			myGPIO.setValue(0);
+			std::cout << "LED OFF" << std::endl;
 		}
 	usleep(500000);
 	}
